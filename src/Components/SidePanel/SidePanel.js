960c723058a23707418817df694eef0307bb62d9
Terrell D Lemons <lemonsterrell2021@gmail.com>
2021-07-14 06:45:37 -0700

    Completed: Put DirectMessage Channel Data in Global State, Set as Private Channels

--- src/Components/SidePanel/SidePanel.js
+++ src/Components/SidePanel/SidePanel.js
@@ -6,23 +6,23 @@ import Channels from "./Channels";
 import DirectMessages from "./DirectMessages";
 
 class SidePanel extends React.Component {
-	render() {
-		const { currentUser } = this.props;
+  render() {
+    const { currentUser } = this.props;
 
-		return (
-			<Menu
-				size="large"
-				inverted
-				fixed="left"
-				vertical
-				style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
-			>
-				<UserPanel currentUser={currentUser} />
-				<Channels currentUser={currentUser} />
-				<DirectMessages currentUser={currentUser} />
-			</Menu>
-		);
-	}
+    return (
+      <Menu
+        size="large"
+        inverted
+        fixed="left"
+        vertical
+        style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
+      >
+        <UserPanel currentUser={currentUser} />
+        <Channels currentUser={currentUser} />
+        <DirectMessages currentUser={currentUser} />
+      </Menu>
+    );
+  }
 }
 
 export default SidePanel;
