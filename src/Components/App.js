995a158b5431f04960bbbc54654a8e0861e7f2e5
Terrell D Lemons <lemonsterrell2021@gmail.com>
2021-07-20 14:19:47 -0700

    Completed: Attach Chosen Colors to User Data

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,3 +1,48 @@
+// import React from "react";
+// import { Grid } from "semantic-ui-react";
+// import "./App.css";
+// import { connect } from "react-redux";
+
+// import ColorPanel from "./ColorPanel/ColorPanel";
+// import SidePanel from "./SidePanel/SidePanel";
+// import Messages from "./Messages/Messages";
+// import MetaPanel from "./MetaPanel/MetaPanel";
+
+// const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts }) => (
+// 	<Grid columns="equal" className="app" style={{ background: "#eee" }}>
+// 		<ColorPanel />
+// 		<SidePanel key={currentUser && currentUser.uid} currentUser={currentUser} />
+
+// 		<Grid.Column style={{ marginLeft: 320 }}>
+// 			<Messages
+// 				key={currentChannel && currentChannel.id}
+// 				currentChannel={currentChannel}
+// 				currentUser={currentUser}
+// 				isPrivateChannel={isPrivateChannel}
+// 			/>
+// 		</Grid.Column>
+
+// 		<Grid.Column width={4}>
+// 			<MetaPanel
+// 				key={currentChannel && currentChannel.id}
+// 				userPosts={userPosts}
+// 				currentChannel={currentChannel}
+// 				isPrivateChannel={isPrivateChannel}
+// 			/>
+// 		</Grid.Column>
+// 	</Grid>
+// );
+
+// const mapStateToProps = (state) => ({
+// 	currentUser: state.user.currentUser,
+// 	currentChannel: state.channel.currentChannel,
+// 	isPrivateChannel: state.channel.isPrivateChannel,
+// 	userPosts: state.channel.userPosts,
+// });
+
+// export default connect(mapStateToProps)(App);
+
+
 import React from "react";
 import { Grid } from "semantic-ui-react";
 import "./App.css";
@@ -10,7 +55,10 @@ import MetaPanel from "./MetaPanel/MetaPanel";
 
 const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts }) => (
 	<Grid columns="equal" className="app" style={{ background: "#eee" }}>
-		<ColorPanel />
+		<ColorPanel
+			key={currentUser && currentUser.name}
+			currentUser={currentUser}
+		/>
 		<SidePanel key={currentUser && currentUser.uid} currentUser={currentUser} />
 
 		<Grid.Column style={{ marginLeft: 320 }}>
@@ -24,7 +72,7 @@ const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts }) => (
 
 		<Grid.Column width={4}>
 			<MetaPanel
-				key={currentChannel && currentChannel.id}
+				key={currentChannel && currentChannel.name}
 				userPosts={userPosts}
 				currentChannel={currentChannel}
 				isPrivateChannel={isPrivateChannel}
